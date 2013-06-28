cd86c7e7858a8e6b4e058de656bd8469f40ada86
Craig Booth <craigmbooth@gmail.com>
2013-06-27 23:12:30 -0500

    Strassen's matrix multiplication algorithm

--- README.md
+++ README.md
@@ -7,13 +7,18 @@ checksum_luhn.py
 ----------------
 Luhn's algorithm for computing a checksum for some decimal data.  As used for e.g. the final digit of credit card numbers.
 
+matrix_multiply_strassen.py
+---------------------------
+Strassen's recursive algorithm for the multiplication of two matrices.  O(n^2.808).
+
+
 multiply_recursive_naive.py
 ---------------------------
 A naive recursive algorithm for multiplying two n-digit numbers.  Scales as O(n^2).
 
 multiply_karatsuba.py
 ---------------------
-Optimization of multiply_recursive_naive.py.  Another recursive multiplication algorithm with better scalings.
+Optimization of multiply_recursive_naive.py.  O(n^1.55).  Another recursive multiplication algorithm with better scalings.
 
 sort_mergesort_recursive.py
 ---------------------------
---
+++ matrix_multiply_strassen.py
@@ -0,0 +1,94 @@
+"""
+Strassen's sub-cubic matrix multiplication algorithm.  Scales as
+O(n^2.808).  Beging by considering 2x2 matrix multiplication:
+
+[a b].[e f] = [ae+bg  af+bh]
+[c d] [g h]   [ce+dg  cf+dh]
+
+Now it is the case that if you break a matrix into blocks and operate
+on the blocks that each of the blocks behaves the same way as the full
+matrix.  We can therefore treat this problem recursively by breaking
+the matrix down in factors of two.  Now, doing this the naive way
+results in O(n^3) behaviour as it requires 8 n/2 matrix
+multiplications per step.  Strassen's insight was that this can be
+performed with 7 multiplications (variables M1-M7 calculated below),
+and that these 7 quantities can be combined in different ways to
+reproduce the four quantities on the right-hand side of the equation
+above.
+"""
+
+
+import numpy as np
+
+def strassen(A,B):
+   """
+   Pad arrays up to a power of two and call strassen_step to multiply
+   the arrays.
+
+   INPUT:  Two square 2d numpy arrays, A and B
+   OUTPUT: A single array containing A.B
+   """
+
+   #Pad up to a power of two:
+   n = A.shape[0]
+   next_power_of_2 = 
+
+   Cpad = strassen_step(Apad,Bpad)
+
+   #Strip out the padding and return:
+
+def strassen_step(A,B):
+   """Strassen's sub-cubic algorithm for matrix multiplication.
+   Perform one recursive step."""
+
+   n = A.shape[0]
+   if n == 1:
+       return int(A*B)
+   else:
+       #Extract sub-matrices:
+       Anw = A[:n/2,:n/2]
+       Ane = A[:n/2,n/2:]
+       Asw = A[n/2:,:n/2]
+       Ase = A[n/2:,n/2:]
+       Bnw = B[:n/2,:n/2]
+       Bne = B[:n/2,n/2:]
+       Bsw = B[n/2:,:n/2]
+       Bse = B[n/2:,n/2:]
+
+       M1 = strassen(Anw+Ase,Bnw+Bse)
+       M2 = strassen(Asw+Ase,Bnw)
+       M3 = strassen(Anw,Bne-Bse)
+       M4 = strassen(Ase,Bsw-Bnw)
+       M5 = strassen(Anw+Ane,Bse)
+       M6 = strassen(Asw-Anw,Bnw+Bne)
+       M7 = strassen(Ane-Ase,Bsw+Bse)
+
+       Cnw = M1 + M4 - M5 + M7
+       Cne = M3 + M5
+       Csw = M2 + M4
+       Cse = M1 - M2 + M3 + M6
+
+       #Copy partial results back into C:
+       C = np.zeros((n,n))
+       C[:n/2,:n/2] = Cnw
+       C[:n/2,n/2:] = Cne
+       C[n/2:,:n/2] = Csw
+       C[n/2:,n/2:] = Cse
+
+       return C.astype(int)
+
+def test_strassen():
+
+    for i in range(100):
+        this_n = np.random.randint(500) #size of matrix for this test
+        max_val = np.random.randint(5000)
+        this_n = 128
+        A = np.random.randint(max_val,size=(this_n,this_n))
+        B = np.random.randint(max_val,size=(this_n,this_n))
+        np.testing.assert_array_equal(strassen(A,B), np.dot(A,B)) 
+
+    print "   --test_strassen() tests passed"
+
+if __name__ == "__main__":
+
+    test_strassen()
