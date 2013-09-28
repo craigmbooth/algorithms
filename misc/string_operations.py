8223e5aabc04d7f11872b5b75c8a6dfc506fd3b4
Craig Booth <craigmbooth@gmail.com>
2013-09-28 13:03:57 -0500

    Added removal of consecutive repeating characters for string_operations.py

--- misc/string_operations.py
+++ misc/string_operations.py
@@ -2,10 +2,15 @@ import itertools
 
 thestr = "gghtgedghsdfgtds"
 
+#Find out whether there are any duplicatged characters in a string:
 charcounts = [(key,len(list(v))) for key,v in itertools.groupby(sorted(thestr))]
-
 charcounts.sort(key=lambda x: x[1],reverse=True)
 if filter(lambda x: x[1]>1,charcounts):
     print "Found duplicates"
 else:
     print "No duplicates found"
+
+#Strip out every consecutive repeating character
+thestr = "gghtge   dghssssdfgtdddddd1ds"
+print "".join([key for key,v in itertools.groupby(thestr)])
+
