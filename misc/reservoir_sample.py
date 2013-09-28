3a13e9ab95dcc3cf5693b9ae05bfaa450e1c3107
Craig Booth <craigmbooth@gmail.com>
2013-09-28 12:47:20 -0500

    Added reservoir sample code

---
+++ misc/reservoir_sample.py
@@ -0,0 +1,28 @@
+import random
+
+NSAMPLE = 3
+
+def reservoir_sample(filename):
+    """ Get a completely random sample of NSAMPLE lines from
+    a file of unknown length with only one loop through the
+    file.  THe algorithm is the following:
+       1. The first NSAMPLE items make up the initial sample
+       2. Then for items of index n, generate a random number
+          [0,n] and if n < NSAMPLE replace that element of the
+          sample.
+    """
+
+    sample = []
+    with open(filename,'r') as f:
+        for i,line in enumerate(f):
+            if i < NSAMPLE:
+                sample.append(line)
+            else:
+                k = random.randint(0,i)
+                if k < NSAMPLE:
+                    sample[k] = line
+    return sample
+
+print reservoir_sample("workfile")
+
+
